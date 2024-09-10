56a76ed11d32088113313c16fdac31fd73589fcb
Raphael Magno <150822511+RMagnoRJ@users.noreply.github.com>
2024-09-10 19:26:04 -0300

    Correção de bugs - Aula 02 100% funcional

--- Intro_Js/aula02_modulo.js
+++ Intro_Js/aula02_modulo.js
@@ -4,14 +4,14 @@ let item = {
     value: 'Tomate',
     //quantidade: "5",
     //tipo: "unidade",
-    carrinho: true
+    checked: true
 }
 
 let listaItem = [ item ]
 
 const cadastrando = async  () => {
 
-    const produto = await input({message: "Digite o produto: "})
+    const item = await input({message: "Digite o produto: "})
     // chama a função INPUT importada no módulo INQUIRER
 
     /* let quantidade = "0"
@@ -34,20 +34,21 @@ const cadastrando = async  () => {
         // chama a função INPUT importada no módulo INQUIRER
     }
     */
-    if (produto.length == 0){
-        console.log("\nEscreva um item para cadastrar na lista!\n")
+
+    if (item.length == 0){
+        console.log("Escreva um item para cadastrar na lista!")
         return
         // Caso necessário podemos chamar a função no RETURN CADASTRANDO() de forma
         // que o user ficasse obrigado a digitar um item para a lista estando preso no
         // return função()
     } else {
-        console.log("\nItem cadastrado com sucesso!\n")
+        console.log("Item cadastrado com sucesso!")
     }
 
     listaItem.push(
         // push = add
         // { produto: produto, quantidade: quantidade, tipo: tipo, carrinho: false }
-        { value: produto, carrinho: false }
+        { value: item, checked: false }
     )
 }
 
@@ -56,23 +57,23 @@ const listaDeCompras = async () => {
     const respostas = await checkbox({
         message: "SETAS navega entre os itens ESPAÇO marca/desmarca item ENTER confirma produto no carrinho",
         choices: [...listaItem],
-        instructions: false
+        instructions: false,
     })
 
     if(respostas.length == 0){
-        console.log("\nNenhum item foi selecionado\n")
+        console.log("Nenhum item foi selecionado")
         return
     }
 
     listaItem.forEach((it) => {
-        it.carrinho = false
+        it.checked = false
     })
 
     respostas.forEach((resposta) => {
         const item = listaItem.find((it) => {
             return it.value == resposta
         })
-       item.carrinho = true
+       item.checked = true
     })
 
 }
@@ -83,18 +84,18 @@ const start = async() => {
 
         const opcao = await select({
             // chama o SELECT importado no modulo INQUIRER
-            message: "\n\n***** Menu *****\n\n>",
+            message: "Menu >",
             choices: [
                 {
-                    name: ">> Cadastrar item",
+                    name: "Cadastrar item",
                     value: "cadastrar"
                 },
                 {
-                    name: ">> Apresentar Lista de Compras",
+                    name: "Apresentar Lista de Compras",
                     value: "listar"
                 },
                 {
-                    name: ">> Sair",
+                    name: "Sair",
                     value: "sair"
                 }
             ]
@@ -104,13 +105,13 @@ const start = async() => {
         switch(opcao) {
 
             case "cadastrar":
-                console.log("\n...CADASTRO...\n")
+                //console.log("\n...CADASTRO...\n")
                 await cadastrando()
                 //o await avisa para o JS que ele terá que AGUARDAR a execução da função
                 break
             
             case "listar":
-                console.log("\n...LISTA DE COMPRAS...\n")
+                //console.log("\n...LISTA DE COMPRAS...\n")
                 //console.log(listaItem)
                 await listaDeCompras()
                 break
